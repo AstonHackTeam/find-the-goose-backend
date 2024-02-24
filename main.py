dae43c840cd7957d0b7194139d1c167809ca621c
Hyq0719 <2418011730@qq.com>
2024-02-24 23:57:00 +0000

    google.cloud.speech_v2

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi import FastAPI, HTTPException
+from pathlib import Path
+
 from speech_to_text import transcribe_file
 
 app = FastAPI()
 
+BASE_DIR = ""  # 修改为你的文件存储目录的绝对路径
 
-@app.post("/upload")
-def upload_file(file: UploadFile = File(...)):
-    if not file.filename.endswith('.wav'):
+@app.get("/upload")
+def upload_file(file_name: str):
+    if not file_name.endswith('.wav'):
         raise HTTPException(status_code=400, detail="File format not supported. Please upload a .wav file.")
 
+    file_path = Path(BASE_DIR) / file_name
+    if not file_path.exists() or not file_path.is_file():
+        raise HTTPException(status_code=404, detail="File not found.")
+
     try:
-        file_data = file.file.read()
-        commands = transcribe_file(file_data)
-        return {"commands": commands}
+        with open(file_path, "rb") as file:
+            file_data = file.read()
+            commands = transcribe_file(file_data)
+            return {"commands": commands}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred: {e}")
-    finally:
-        file.file.close()
